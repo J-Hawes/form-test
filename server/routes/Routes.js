e87b3db6160d30e67daf3bdc0886f074d744e837
J-Hawes <91867589+J-Hawes@users.noreply.github.com>
2022-03-07 13:00:24 +1100

    added backend

---
+++ server/routes/Routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+// //required files
+// const apiController = require("../api/Controllers");
+
+// //Calls to controllers
+// router.post("/property-report", apiController.getPropertyReport);
+
+// router.post("/movers", apiController.getMovers);
+
+module.exports = router;
